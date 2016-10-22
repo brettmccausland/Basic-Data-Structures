0463842f656a328e7e867f64cfd21a1f4dfd1f60
brettmccausland <brettmccausland@yahoo.com>
2016-10-21 20:32:09 -0700

    Included Exercises and Directions

---
+++ main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+#include <vector>
+void Exercise12_1();
+void Exercise12_3();
+void Exercise12_4();
+void Exercise12_6();
+void Exercise12_9();
+void Exercise12_15();
+void Exercise12_16();
+using namespace std;
+int main()
+{
+  system("clear");
+
+
+
+  return 0;
+}
+void Exercise12_1()
+{
+  //Write a function
+  // void downsize(list<string>& names)
+  // that removes every second value from a linked list.
+}
+void Exercise12_3()
+{
+  // Write a function sort that sorts the elements of a
+  // linked list (without
+  // copying them into a vector).
+}
+void Exercise12_4()
+{
+  //Write a function merge that merges two lists into one,
+  //alternating ele- ments from each list until the end of one
+  //of the lists has been reached, then appending the
+  //remaining elements of the other list. For example,
+  //merging the lists containing A B C and D E F G H
+  //should yield the list A D B E C F G H.
+}
+void Exercise12_6()
+{
+  // Write a member function
+  // List::reverse()
+   //that reverses the nodes in a list.
+}
+void Exercise12_9()
+{
+  // Write a member function
+  // List::get_size()
+  // that computes the num- ber of elements in the list,
+  // by counting the elements until the end of the list
+  // is reached.
+}
+void Exercise12_15()
+{
+  //Implement a Stack class, using a linked list of strings. Supply
+  //operations size, push, pop, and top, just like in the standard
+  //stack template.
+}
+void Exercise12_16()
+{
+  //Implement a Queue class, using a linked list of strings.
+  //Supply operations size, push, pop, front, and back,
+  //just like in the standard stack template.
+}
